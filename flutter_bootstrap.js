15ace49fc1c6403daa49fcc935e9166947715544
Zinc <qianyuxin2003@gamil.com>
2024-06-28 14:39:43 +0800

    update5

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3521807308"
+    serviceWorkerVersion: "4103146587"
   }
 });
